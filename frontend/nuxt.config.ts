0f8ca3ba2a7c931644c360be14a3a416a9d199ae
saplyn <saplynx@outlook.com>
2024-05-01 12:19:06 +0800

    dep: tailwind & primevue

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+      unstyled: false,
+      ripple: true,
+    },
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  },
+  css: [
+    "assets/css/tailwind.css",
+    "primevue/resources/themes/lara-light-green/theme.css",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ frontend/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2 class="bg-red-500">Page/index.vue</h2>
+    <Button>uwu</Button>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
