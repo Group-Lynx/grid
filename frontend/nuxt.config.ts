98a1334ac92a6ba0767c5888469dc91182d69113
saplyn <saplynx@outlook.com>
2024-05-17 17:50:23 +0800

    add vueuse module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   primevue: {
     usePrimeVue: true,
     options: {
